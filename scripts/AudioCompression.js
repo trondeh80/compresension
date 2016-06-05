90d8aec297d8fcd20cf16f45bfe67cd81d9f8157
trondeh80 <trond.erling@gmail.com>
2016-06-05 20:39:05 +0200

    Added some visualization of the audio data for the active tab.

--- scripts/AudioCompression.js
+++ scripts/AudioCompression.js
@@ -20,149 +20,163 @@ Compressor.prototype.getGain = getGain;
 Compressor.prototype.setGainOptions = setGainOptions;
 Compressor.prototype.setGainAmount = setGainAmount;
 Compressor.prototype.getGainAmount = getGainAmount;
+Compressor.prototype.getAnalyseData = getAnalyseData;
 
 function CompressorConstructor(Extension, msg) {
-  this.isEnabled = false ;
-  this.isActive = false;
-  this.extension = Extension ;
+    this.isEnabled = false;
+    this.isActive = false;
+    this.extension = Extension;
 }
 
 function messageEvent(msg) {
-  if (typeof this[msg['action']] === "function") {
-    this[msg['action']].call(this, msg.args);
-  }
+    if (typeof this[msg['action']] === "function") {
+        this[msg['action']].call(this, msg.args);
+    }
 }
 
-function getPort(){
-  return this.extension.getPort();
+function getPort() {
+    return this.extension.getPort();
 }
 
 function sendMessage(msg) {
-  this.getPort().postMessage(msg);
+    this.getPort().postMessage(msg);
 }
 
 function captureAudio(msg) {
-  this.setProperties(msg.compression);
-  chrome.tabs.getSelected(null, function (tab) {
-    this.tab = tab ;
-    if (!this.isEnabled) {
-      chrome.tabCapture.capture({
-        audio: true,
-        video: false
-      }, activateAudioCompression.bind(this));
-    } else {
-      this.reactivateCompression();
-    }
-  }.bind(this));
+    this.setProperties(msg.compression);
+    chrome.tabs.getSelected(null, function (tab) {
+        this.tab = tab;
+        if (!this.isEnabled) {
+            chrome.tabCapture.capture({
+                audio: true,
+                video: false
+            }, activateAudioCompression.bind(this));
+        } else {
+            this.reactivateCompression();
+        }
+    }.bind(this));
 }
 
 function getContext() {
-  if (!this.context) {
-    this.context = new AudioContext();
-  }
-  return this.context;
+    if (!this.context) {
+        this.context = new AudioContext();
+    }
+    return this.context;
 }
 
 function getCompression() {
-  if (!this.compressor) {
-    this.compressor = this.getContext().createDynamicsCompressor();
-  }
-  return this.compressor;
+    if (!this.compressor) {
+        this.compressor = this.getContext().createDynamicsCompressor();
+    }
+    return this.compressor;
 }
 
 function setCompressionOptions(properties) {
-  this.setProperties(properties) ;
-  this.getCompression().threshold.value = properties.threshold;
-  this.getCompression().knee.value = properties.knee;
-  this.getCompression().ratio.value = properties.ratio;
-  this.getCompression().reduction.value = properties.reduction;
-  this.getCompression().attack.value = properties.attack;
-  this.getCompression().release.value = properties.release;
+    this.setProperties(properties);
+    this.getCompression().threshold.value = properties.threshold;
+    this.getCompression().knee.value = properties.knee;
+    this.getCompression().ratio.value = properties.ratio;
+    this.getCompression().reduction.value = properties.reduction;
+    this.getCompression().attack.value = properties.attack;
+    this.getCompression().release.value = properties.release;
 }
 
-function updateCompression(msg){
-  this.setCompressionOptions(msg.compression) ;
-  this.setGainOptions(msg.gain);
+function updateCompression(msg) {
+    this.setCompressionOptions(msg.compression);
+    this.setGainOptions(msg.gain);
 }
 
 function setGainOptions(gain) {
-  this.setGainAmount(gain);
-  this.getGain().gain.value = gain;
+    this.setGainAmount(gain);
+    this.getGain().gain.value = gain;
 }
 
 function activateAudioCompression(mediaSource) {
-  this.source = this.getContext().createMediaStreamSource(mediaSource);
-  this.setCompressionOptions(this.getProperties());
-  this.source.connect(this.getCompression());
+    this.source = this.getContext().createMediaStreamSource(mediaSource);
+    this.setCompressionOptions(this.getProperties());
+    this.source.connect(this.getCompression());
 
-  // Original working (only compressor).
-  // this.getCompression().connect(this.getContext().destination);
+    // Original working (only compressor).
+    // this.getCompression().connect(this.getContext().destination);
 
-  // With gain
-  this.getCompression().connect(this.getGain()) ;
-  this.getGain().connect(this.getAnalyser()) ;
-  this.getAnalyser().connect(this.getContext().destination) ;
+    // With gain
+    this.getCompression().connect(this.getGain());
+    this.getGain().connect(this.getAnalyser());
+    this.getAnalyser().connect(this.getContext().destination);
 
-  // with only visualizer
-  // this.getCompression().connect(this.getAnalyser());
-  // this.getAnalyser().connect(this.getContext().destination) ;
+    // with only visualizer
+    // this.getCompression().connect(this.getAnalyser());
+    // this.getAnalyser().connect(this.getContext().destination) ;
 
-  this.isActive = true;
-  this.isEnabled = true;
+    this.isActive = true;
+    this.isEnabled = true;
 }
 
 function getGain() {
-  if (!this.gain) {
-    this.gain = this.getContext().createGain();
-  }
-  return this.gain;
+    if (!this.gain) {
+        this.gain = this.getContext().createGain();
+    }
+    return this.gain;
 }
 
-function getAnalyser(){
-  if (!this.analyser) {
-    this.analyser = this.getContext().createAnalyser();
-  }
-  return this.analyser;
+function getAnalyser() {
+    if (!this.analyser) {
+        this.analyser = this.getContext().createAnalyser();
+        this.analyser.fftSize = 2048;
+        this.analyseData = new Uint8Array(this.analyser.frequencyBinCount);
+    }
+    return this.analyser;
 }
 
-function deactivateCompression(){
-  this.source.disconnect(this.getCompression());
-  this.getAnalyser().disconnect(this.getContext().destination);
-  this.source.connect(this.getContext().destination);
+function getAnalyseData() {
+    this.getAnalyser().getByteTimeDomainData(this.analyseData);
+    this.sendMessage({
+        action: 'showAnalyse',
+        args: {
+            data: this.analyseData,
+            bufferLength: this.getAnalyser().frequencyBinCount
+        }
+    })
 }
 
-function reactivateCompression(){
-  this.source.connect(this.getCompression());
-  this.getAnalyser().connect(this.getContext().destination);
-  this.isActive = true;
+function deactivateCompression() {
+    this.source.disconnect(this.getCompression());
+    this.getAnalyser().disconnect(this.getContext().destination);
+    this.source.connect(this.getContext().destination);
+}
+
+function reactivateCompression() {
+    this.source.connect(this.getCompression());
+    this.getAnalyser().connect(this.getContext().destination);
+    this.isActive = true;
 }
 
 function getProperties() {
-  return this.properties || (this.properties = {
-      threshold: -50,
-      knee: 40,
-      ratio: 12,
-      reduction: -40,
-      attack: 0,
-      release: 0.25
-    });
+    return this.properties || (this.properties = {
+            threshold: -50,
+            knee: 40,
+            ratio: 12,
+            reduction: -40,
+            attack: 0,
+            release: 0.25
+        });
 }
 
-function getGainAmount(){
-  return this.gainAmount || (this.gainAmount = 0.1) ;
+function getGainAmount() {
+    return this.gainAmount || (this.gainAmount = 0.1);
 }
 
-function setGainAmount(_gain){
-  this.gainAmount = _gain;
+function setGainAmount(_gain) {
+    this.gainAmount = _gain;
 }
 
 function setProperties(_properties) {
-  this.properties = _properties;
+    this.properties = _properties;
 }
 
 function getCompressionOptions() {
-  this.sendMessage({
-    action: 'setOptions',
-    args: {compression: this.getProperties(), gain:this.getGainAmount(), isActive: this.isActive}
-  });
+    this.sendMessage({
+        action: 'setOptions',
+        args: {compression: this.getProperties(), gain: this.getGainAmount(), isActive: this.isActive}
+    });
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,110 +1,170 @@
 'use strict';
-var App = appConstructor ;
-App.prototype.sendMessage = sendMessage ;
-App.prototype.addListeners = addListeners ;
+var App = appConstructor;
+App.prototype.sendMessage = sendMessage;
+App.prototype.addListeners = addListeners;
 App.prototype.getPort = getPort;
-App.prototype.messageEvent = messageEvent ;
+App.prototype.messageEvent = messageEvent;
 App.prototype.activate = activate;
 App.prototype.deactivate = deactivate;
 App.prototype.setOptions = setOptions;
 App.prototype.getGain = getGain;
 App.prototype.getCompressionOptions = getCompressionOptions;
+App.prototype.collectData = collectData;
+App.prototype.showAnalyse = showAnalyse;
+App.prototype.getCanvasContext = getCanvasContext;
+App.prototype.getCanvas = getCanvas;
 
 var application = new App();
 
-function appConstructor(){
-  this.getPort().onMessage.addListener(this.messageEvent.bind(this)) ;
-  $(document).ready(addListeners.bind(this)) ;
+function appConstructor() {
+    this.getPort().onMessage.addListener(this.messageEvent.bind(this));
+    $(document).ready(addListeners.bind(this));
+    //
+    //   // A bit hacky
+    // this.canvas = document.getElementById('analyse') ;
+    // this.context = this.canvas.getContext("2d");
 
-  this.sendMessage({
-    action:'getCompressionOptions'
-  });
+    this.sendMessage({
+        action: 'getCompressionOptions'
+    });
 }
 
-function setOptions(obj){
-  this.compression = obj.compression ;
-  this.gain = obj.gain ;
-
-  _(this.compression).each(function(item, key){
-    $('#'+key).val(item) ;
-  });
-
-  $("#gain").val(this.gain);
-
-  if (obj.isActive) {
-    $('#activate').addClass('hidden') ;
-    $('#deactivate').removeClass('hidden') ;
-  }else {
-    $('#activate').removeClass('hidden') ;
-    $('#deactivate').addClass('hidden') ;
-  }
-}
-
-function addListeners(event){
-  document.getElementById('activate').addEventListener('click', activate.bind(this)) ;
-  document.getElementById('deactivate').addEventListener('click', deactivate.bind(this)) ;
-
-  $('form input').change(activateChanges.bind(this)) ;
-}
-
-function getCompressionOptions(){
-  _(this.compression).each(function(item, key){
-    this.compression[key] = $('#'+key).val() ;
-  }.bind(this));
-  return this.compression;
-}
-
-function getGain(){
-  return $('#gain').val();
-}
-
-function activateChanges(){
-   this.sendMessage({
-    action:'updateCompression',
-    args:{
-      compression:this.getCompressionOptions(),
-      gain:getGain()
+function getCanvas() {
+    if (!this.canvas) {
+        this.canvas = document.getElementById('analyser');
     }
-  }) ;
+    return this.canvas;
 }
 
-function activate(){
-  this.sendMessage({
-    action:'captureAudio',
-    args:{
-      compression:this.getCompressionOptions(),
-      gain:this.getGain()
+function getCanvasContext() {
+    if (!this.context) {
+        this.context = this.getCanvas().getContext("2d");
     }
-  }) ;
-  $('#activate').addClass('hidden') ;
-  $('#deactivate').removeClass('hidden') ;
+    return this.context;
 }
 
-function deactivate(){
-  this.sendMessage({
-    action:'deactivateCompression'
-  }) ;
-  $('#activate').removeClass('hidden') ;
-  $('#deactivate').addClass('hidden') ;
+function setOptions(obj) {
+    this.compression = obj.compression;
+    this.gain = obj.gain;
+
+    // Setting the options for compression:
+    _(this.compression).each(function (item, key) {
+        $('#' + key).val(item);
+    });
+
+    $("#gain").val(this.gain);
+
+    if (obj.isActive) {
+        $('#activate').addClass('hidden');
+        $('#deactivate').removeClass('hidden');
+    } else {
+        $('#activate').removeClass('hidden');
+        $('#deactivate').addClass('hidden');
+    }
 }
 
-function getPort(){
-  if (!this.port) {
-    this.port = chrome.extension.connect({name: "communicator"});
-  }
-  return this.port ;
+function addListeners(event) {
+    document.getElementById('activate').addEventListener('click', activate.bind(this));
+    document.getElementById('deactivate').addEventListener('click', deactivate.bind(this));
+
+    $('form input').change(activateChanges.bind(this));
 }
 
-function sendMessage(msg){
-  chrome.tabs.getSelected(null, function (tab) {
-    msg.tabId = tab.id ;
-    this.getPort().postMessage(msg) ;
-  }.bind(this));
+function getCompressionOptions() {
+    _(this.compression).each(function (item, key) {
+        this.compression[key] = $('#' + key).val();
+    }.bind(this));
+    return this.compression;
+}
+
+function getGain() {
+    return $('#gain').val();
+}
+
+function activateChanges() {
+    this.sendMessage({
+        action: 'updateCompression',
+        args: {
+            compression: this.getCompressionOptions(),
+            gain: getGain()
+        }
+    });
+}
+
+function activate() {
+    this.sendMessage({
+        action: 'captureAudio',
+        args: {
+            compression: this.getCompressionOptions(),
+            gain: this.getGain()
+        }
+    });
+
+    this.analyseTicker = setInterval(this.collectData.bind(this), 200);
+
+    $('#activate').addClass('hidden');
+    $('#deactivate').removeClass('hidden');
+}
+
+function collectData() {
+    this.sendMessage({
+        action: 'getAnalyseData'
+    });
+}
+
+function showAnalyse(args) {
+    var WIDTH = this.getCanvas().scrollWidth;
+    var HEIGHT = 75;
+    this.getCanvasContext().clearRect(0, 0, WIDTH, HEIGHT);
+
+    var barWidth = (WIDTH / args.bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+    for (var i = 0; i < args.bufferLength; i++) {
+        barHeight = args.data[i] / 2;
+        this.getCanvasContext().fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+        this.getCanvasContext().fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
+        x += barWidth + 1;
+    }
+
+    // window.requestAnimationFrame(function () {
+    //     var barWidth = (WIDTH / args.bufferLength) * 2.5;
+    //     var barHeight;
+    //     var x = 0;
+    //     for (var i = 0; i < args.bufferLength; i++) {
+    //         barHeight = args.data[i] / 2;
+    //         this.getCanvasContext().fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+    //         this.getCanvasContext().fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
+    //         x += barWidth + 1;
+    //     }
+    // }.bind(this));
+}
+
+function deactivate() {
+    this.sendMessage({
+        action: 'deactivateCompression'
+    });
+    $('#activate').removeClass('hidden');
+    $('#deactivate').addClass('hidden');
+}
+
+function getPort() {
+    if (!this.port) {
+        this.port = chrome.extension.connect({name: "communicator"});
+    }
+    return this.port;
+}
+
+function sendMessage(msg) {
+    chrome.tabs.getSelected(null, function (tab) {
+        msg.tabId = tab.id;
+        this.getPort().postMessage(msg);
+    }.bind(this));
 
 }
 
-function messageEvent(msg){
-  if (typeof this[msg['action']] === 'function') {
-    this[msg['action']].call(this,msg.args);
-  }
+function messageEvent(msg) {
+    if (typeof this[msg['action']] === 'function') {
+        this[msg['action']].call(this, msg.args);
+    }
 }
